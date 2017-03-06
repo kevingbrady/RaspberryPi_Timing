086f94d044ce76a4972b488cf6d68de3a58da697
kevingbrady <kgb@nist.gov>
2017-03-06 12:24:44 -0500

    Added comments

--- RPi_IRIGB_runner.py
+++ RPi_IRIGB_runner.py
@@ -8,7 +8,7 @@ sys.stdout = filename
 
 pin = 12
 time_end = datetime.datetime.now() + datetime.timedelta(hours=12)   #How long you want it to run
-time_end += datetime.timedelta(seconds=3)			     #It takes 3 seconds to adjust to the pulse and then starts
+time_end += datetime.timedelta(seconds=4)			     #it takes 3 seconds to start so add 4 seconds to get an even dataset
 p = IRIGB.Offset_Decoder()
 count = 0
 pi = ''
@@ -16,12 +16,12 @@ irig = ''
 
 while(datetime.datetime.now() <= time_end):
 	
-	pi = p.pi
-	irig = p.get_decoded_data(pin)
+	pi = p.pi							# collect pi timestamp value
+	irig = p.get_decoded_data(pin)					# collect decoded IRIG-B timestamp
 
-	if pi == '':
-		pass
-	else:		
+	if pi == '':							# if there is no value for pi pass (the first value always 
+		pass							# reads in wrong since it starts in the middle of a pulse most of the time 
+	else:								# so this avoids printing output until a full pulse has occured)
 		if p.clock == '':
 
 			print str(irig) + '   |   ' + str(pi)
@@ -30,7 +30,7 @@ while(datetime.datetime.now() <= time_end):
 
 			p.clock = ''
 			if count > 2:
-				print str(irig) + '   |   ' +  str(pi)
+				print str(irig) + '   |   ' +  str(pi)	
 
 	count += 1
 
