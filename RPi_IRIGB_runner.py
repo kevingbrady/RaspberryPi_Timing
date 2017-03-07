0656b9fd34618866dd62b612b035d7f7672960ee
kevingbrady <kgb@nist.gov>
2017-03-07 11:52:25 -0500

    Runs RPi_IRIGB_Offset code
    Changed from 2 seconds to 3 to get even datasets after every run

--- RPi_IRIGB_runner.py
+++ RPi_IRIGB_runner.py
@@ -3,12 +3,12 @@ import RPi_IRIGB_offset as IRIGB
 import datetime
 import sys
 
-filename = open('(3-01-17)to(3-02-17).txt', 'w')
+filename = open('Pulse_Reader.txt', 'w')
 sys.stdout = filename
 
 pin = 12
 time_end = datetime.datetime.now() + datetime.timedelta(hours=12)   #How long you want it to run
-time_end += datetime.timedelta(seconds=2)			     #it takes 2 seconds to start so add 2 seconds to get an even dataset
+time_end += datetime.timedelta(seconds=3)			     #it takes 3 seconds to start so add 3 seconds to get an even dataset
 p = IRIGB.Offset_Decoder()
 count = 0
 pi = ''
