0ace4434366f06da2d8ddd85b1f3c5a155a7c4e0
kevingbrady <kgb@nist.gov>
2017-05-08 09:55:56 -0400

    Add files via upload

--- RPi_IRIGB_offset.py
+++ RPi_IRIGB_offset.py
@@ -2,10 +2,11 @@
 import datetime
 import RPi.GPIO as GPIO
 import time
+import subprocess
 
-class Offset_Decoder():
-
-    daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]         
+class Decoder():
+	
+    daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     Seconds = 0.0
     Minutes = 0
     Hours = 0
@@ -20,182 +21,204 @@ class Offset_Decoder():
     Parity = ''
     CTQ = ''
     SBS = 0.0
+    piFlag = 0
     pi = ''
-    piFlag = 1
-    clock = ''
+    total = 0
+    clocktime = ''
+    templist = []
+    numbits = []
+    bits = []
+    bitcount = 0
+    q = []
 
     def __init__(self):
+
         self.data = []
 
-    def setup(self, pin):                           # sets up GPIO pin
+    def setup(self,pin):
+
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(pin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
-    def current_microseconds(self):                 # calculates current timestamp in microseconds
-        current_microseconds = int(round(time.time() * 1000000))
-        return current_microseconds
-    
-    def pulse_microseconds(self, pin):              # reads in pulse and calculates the number of microseconds bewteen high and low input values
-        ptime = 0
+    def current_microseconds(self):
+
+        microseconds = time.time() * 1000000
+        return microseconds
+
+    def pulse_microseconds(self, pin):
+
         starttime = self.current_microseconds()
-        while (GPIO.input(pin)):
-            if (GPIO.input(pin) == False):
+        ptime = 0
+        total_microseconds = 0
+
+        while GPIO.input(pin) == GPIO.HIGH:
+
+            ptime = self.current_microseconds()
+            if(ptime - starttime) > 8300:
+
                 break
+
             else:
-                ptime = self.current_microseconds()
 
-        total_microseconds = (ptime - starttime)
-        return total_microseconds
+                total_microseconds = int(round((ptime - starttime)/500))* 500
+                
+       
+        return total_microseconds 
 
-    def bin_seconds_today(self, today):                 # calculates binary seconds of current day based on date enetred in Y-m-d format
+    def bin_seconds_today(self, today):
 
         today = datetime.datetime.strptime(today, '%Y-%m-%d')
-        epoch = datetime.datetime.strptime('1970-1-1', '%Y-%m-%d')
-        if today > epoch:
+        epoch = 0.0
+        max_year = 253402232400.0
 
-            sec = time.mktime(today.timetuple())
-            return sec
+        if epoch < today.timestamp() < max_year:
 
-    def fix_bits(self, templist):                           # checks if the pointer bits are at the correct indexes, if not then
-        try:                                                # append or delete values if there are an incorrect number of bits
-            z = 0                                           # between each pointer bit
-            i = 8                                           # sets check equal to 1 if input is normal
-            c = 0
-            while z < len(templist) - 1:
+            sec = today - datetime.datetime.fromtimestamp(epoch)
+            return sec.total_seconds()
+      
+    def irig_decoder(self, pulsebits):
 
-                if templist[z] == 2:
-                    if z == i:
-                        c += 1
-                    else:
-                        if z < i:
-                            while z != i:
-                                templist.insert(z, 0)
-                                z += 1
-                        elif z > i:
-                            while z != i:
-                                del templist[z - 1]
-                                z -= 1
-                    i += 10
-                z += 1
-                if c == 10:
-                    check = 1
-                    return check
-        except IndexError:
-            pass
-
-    def irig_decoder(self, pulsebits):              # decodes IRIG-B timestamp based on the indexes of the bits in pulsebits
+        third = (len(pulsebits)) - 63        
         x = 0
         self.Month = 0
         self.Day = 0
+ 
+        self.DaysOY = (pulsebits[third] * 1) + (pulsebits[third +1] * 2) + (pulsebits[third +2] * 4) + (pulsebits[third + 3] * 8) + (pulsebits[third + 5] * 10) + (pulsebits[third + 6] * 20) + (pulsebits[third + 7] * 40) + (pulsebits[third + 8] * 80) + (pulsebits[third + 9] * 100) +(pulsebits[third + 10] * 200)
+        self.Year = ((pulsebits[third + 18] * 1) + (pulsebits[third + 19] * 2) + (pulsebits[third + 20] * 4) + (pulsebits[third + 21] * 8) + (pulsebits[third + 23] * 10) + (pulsebits[third + 24] * 20) + (pulsebits[third + 25] * 40) + (pulsebits[third + 26] * 80)) + 2000
+        self.SBS = float((pulsebits[third + 45] * (2**0)) + (pulsebits[third + 46] * (2**1)) + (pulsebits[third + 47] * (2**2)) + (pulsebits[third + 48] * (2**3)) + (pulsebits[third + 49] * (2**4)) + (pulsebits[third + 50] * (2**5)) + (pulsebits[third + 51] * (2**6)) + (pulsebits[third + 52] * (2**7)) + (pulsebits[third + 53] * (2**8)) + (pulsebits[third + 54] * (2**9)) + (pulsebits[third + 55] * (2**10)) + (pulsebits[third + 56] * (2**11)) + (pulsebits[third + 57] * (2**12)) + (pulsebits[third + 58] * (2**13)) + (pulsebits[third + 59] * (2**14)) + (pulsebits[third + 60] * (2**15)) + (pulsebits[third + 61] * (2**16)))
+        #self.Leap_Second = '(' + str(pulsebits[third + 27]) + str(pulsebits[third + 28]) +')'
+        #self.DST = '(' + str(pulsebits[third + 29]) + str(pulsebits[third + 30]) + ')'
+        #self.Time_Offset = '(' + str(pulsebits[third + 31])+ str(pulsebits[third + 32]) + str(pulsebits[third + 33]) + str(pulsebits[third + 34]) + str(pulsebits[third + 35]) + str(pulsebits[third + 36]) + ')' 
+        #self.Time_Quality = '(' + str(pulsebits[third + 37]) + str(pulsebits[third + 38]) + str(pulsebits[third + 39]) + str(pulsebits[third + 40])+ ')'
+        #self.Parity = str(pulsebits[third + 41])
+        #self.CTQ = '(' + str(pulsebits[third + 42]) + str(pulsebits[third + 43]) + str(pulsebits[third + 44]) + ')'
 
-        self.Seconds = float((pulsebits[0] * 1) + (pulsebits[1] * 2) + (pulsebits[2] * 4) + (pulsebits[3] * 8) + (pulsebits[5] * 10) + (pulsebits[6] * 20) + (pulsebits[7] * 40))
-        self.Minutes = (pulsebits[8] * 1) + (pulsebits[9] * 2) + (pulsebits[10] * 4) + (pulsebits[11] * 8) + (pulsebits[13] * 10) + (pulsebits[14] * 20) + (pulsebits[15] * 40)
-        self.Hours =  (pulsebits[17] * 1) + (pulsebits[18] * 2) + (pulsebits[19] * 4) + (pulsebits[20] * 8) + (pulsebits[22] * 10) + (pulsebits[23] * 20)
-        self.DaysOY = (pulsebits[26] * 1) + (pulsebits[27] * 2) + (pulsebits[28] * 4) + (pulsebits[29] * 8) + (
-        pulsebits[31] * 10) + (pulsebits[32] * 20) + (pulsebits[33] * 40) + (pulsebits[34] * 80) + (
-                      pulsebits[35] * 100) + (pulsebits[36] * 200)
-        self.Year = ((pulsebits[44] * 1) + (pulsebits[45] * 2) + (pulsebits[46] * 4) + (pulsebits[47] * 8) + (
-        pulsebits[49] * 10) + (pulsebits[50] * 20) + (pulsebits[51] * 40) + (pulsebits[52] * 80)) + 2000
-        self.SBS = float((pulsebits[71] * (2 ** 0)) + (pulsebits[72] * (2 ** 1)) + (pulsebits[73] * (2 ** 2)) + (
-        pulsebits[74] * (2 ** 3)) + (pulsebits[75] * (2 ** 4)) + (pulsebits[76] * (2 ** 5)) + (
-                         pulsebits[77] * (2 ** 6)) + (pulsebits[78] * (2 ** 7)) + (pulsebits[79] * (2 ** 8)) + (
-                         pulsebits[80] * (2 ** 9)) + (pulsebits[81] * (2 ** 10)) + (pulsebits[82] * (2 ** 11)) + (
-                         pulsebits[83] * (2 ** 12)) + (pulsebits[84] * (2 ** 13)) + (pulsebits[85] * (2 ** 14)) + (
-                         pulsebits[86] * (2 ** 15)) + (pulsebits[87] * (2 ** 16)))
-        self.Leap_Second = '(' + str(pulsebits[53]) + str(pulsebits[54]) + ')'
-        self.DST = '(' + str(pulsebits[55]) + str(pulsebits[56]) + ')'
-        self.Time_Offset = '(' + str(pulsebits[57]) + str(pulsebits[58]) + str(pulsebits[59]) + str(
-            pulsebits[60]) + str(pulsebits[61]) + str(pulsebits[62]) + ')'
-        self.Time_Quality = '(' + str(pulsebits[63]) + str(pulsebits[64]) + str(pulsebits[65]) + str(
-            pulsebits[66]) + ')'
-        self.Parity = str(pulsebits[67])
-        self.CTQ = '(' + str(pulsebits[68]) + str(pulsebits[69]) + str(pulsebits[70]) + ')'
+        if(self.Year % 4 == 0):
 
-        if (self.Year % 4 == 0):                        #check for leap year and change daysInMonth if it is
-            if (self.Year % 100 != 0):
+            if(self.Year % 100 != 0):
+
                 self.daysInMonth[1] = 29
-            elif (self.Year % 400 == 0):
+
+            elif(self.Year % 400 == 0):
+
                 self.daysInMonth[1] = 29
+
+            else:
+
+                pass
+
         else:
-            pass
+
+            self.daysInMonth[1] = 28
 
         self.Day = self.DaysOY
 
-        while (self.Month < 12):                        # calculate the month and the day from Days of the Year
+        while(self.Month < 12):
+
             self.Month += 1
-            if (self.Day > self.daysInMonth[x]):
+
+            if(self.Day > self.daysInMonth[x]):
+
                 self.Day -= self.daysInMonth[x]
                 x += 1
+               
+
             else:
-                if (self.Day == 0):
+                
+                if(self.Day == 0):
+
                     self.Day += 1
+
                 break
-                                                        
-                                                        # calculates the binary seconds for the current day at 24:00 and adds this to the SBS
-                                                        # decoded from pulsebits to get the correct decoded value as a number
-        timestamp = self.bin_seconds_today(str(self.Year) + '-' + str(self.Month) + '-' + str(self.Day)) + self.SBS
-        Time = str(datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S.%f'))   # puts numerical timestamp in datetime format
+
+        timestamp = self.bin_seconds_today(str(self.Year) + '-' + str(self.Month) + '-' + str(self.Day)) + self.SBS		
+        Time = datetime.datetime.utcfromtimestamp(timestamp)
         
         return Time
 
+    def reset(self):
+
+        self.piFlag = 0
+        self.total = 0
+        self.clocktime = ''
+
+    def clearlists(self):
+
+        del self.templist[:]
+        del self.bits[:]
+        del self.numbits[:]
+        self.bitcount = 0
+
     def get_decoded_data(self, pin):
-        self.setup(pin)                 #sets up pin for input
-        field = []
-        total = []
-        templist = []
-        pointer = 1
-        clocktime = ''
 
+        self.setup(pin)
         while True:
-            microseconds = 0                                        # resets microseconds (keeps from a value carrying over in the loop)
-            microseconds = self.pulse_microseconds(pin)             # reads in the width of the pulse (100 per second for IRIG-B)
-            if microseconds > 200:
+            
+            microseconds = self.pulse_microseconds(pin)
 
-                if (microseconds <= 3200):                          # appends a 0, 1, or 2 based on the width of the pulse
-                    templist.append(0)
-                elif (microseconds <= 7200):
-                    templist.append(1)
-                elif (microseconds <= 9200):
-                    templist.append(2)
+            if microseconds < 1:
+        
+                pass
+
+            elif(1000 <= microseconds <= 3000):
+
+                if self.bitcount < 9:
+                    self.templist.append(0)
+                    self.bitcount += 1
+
                 else:
-                    pass
 
-                if templist[-1] == 2:                               # TIMESTAMP RASPBERRY PI
-                    if pointer != 10:                               # keeps track of number of pointer bits that have come in and timestamps when it recognizes the P0 bit (on-time reference bit)
-                        if pointer == 0:                            # Sets piFlag = 1 to ensure that the Raspberry Pi only timestamps once
-                            if self.piFlag == 0:
-                                self.pi = datetime.datetime.utcfromtimestamp(time.time())
-                                self.piFlag = 1
-                            pointer = 1
-                        else:            
-                            pointer += 1
-                    else:
-                        pointer = 0
-                        self.piFlag = 0
+                    microseconds = 8000
 
-                    if templist[-2:] == [2, 2]:                     # DECODE IRIGB
-                                                                    # If the length of templist is 100 then run the checker to ensure that there are not any input errors
-                        if len(templist) == 100:            
-                            checkFlag = self.fix_bits(templist)
-                        else:                                       # If length is not 100 there are input errors so run checker to fix them
-                            self.clock = datetime.datetime.utcfromtimestamp(time.time())
-                            self.fix_bits(templist)
-                            checkFlag = self.fix_bits(templist)
+            elif(5000 <= microseconds <= 6000):
 
-                        for a, b, c in zip(templist, templist[1:], templist[2:]):
+                if self.bitcount < 9:
 
-                            if (a == 0 or a == 1):                  # Run through templist and append the bits to a new list that excludes the pointer bits
-                                field.append(a)
-                                if (b == 2):
-                                    total += field
-                                    del field[:]
-                                    if (c == 2):
-                                        if (len(total) == 89 and checkFlag == 1):       # If total is the correct length and
-                                            clocktime = str(self.irig_decoder(total))   # there are not input errors run the decoder on total
-                                        del total[:]
-                                        del templist[:]                                 # delete values in both lists to avoid them carrying over 
-                        if clocktime == '':
-                            pass
-                        else:
-                            return clocktime                                            # do not return a value unless there is one to return 
-                       
+                    self.templist.append(1)
+                    self.bitcount += 1
+
+                else:
+
+                    microseconds = 8000
+
+            elif(microseconds == 8000):
+
+                self.templist.append(2)
+                self.numbits.append(self.bitcount)
+                self.bitcount = 0
+
+            if microseconds > 1:
+
+                self.q.append(microseconds)
+                if len(self.q) > 100:
+
+                    del self.q[0]
+            
+            if self.templist[-2:] == [2,2]:
+
+                if self.piFlag != 1:
+
+                            self.pi = datetime.datetime.utcfromtimestamp(time.time())
+                            self.piFlag = 1
+
+                if len(self.templist) > 70:
+                        
+                    for i in self.templist:
+
+                        if i == 0 or i == 1:
+
+                            self.bits.append(i)
+
+                    self.clocktime = self.irig_decoder(self.bits)
+                
+                else:
+
+                    self.clearlists()
+                    self.reset()
+
+            if self.clocktime != '':
+
+                break
+
+                    
+  
--- RPi_IRIGB_runner.py
+++ RPi_IRIGB_runner.py
@@ -1,37 +1,54 @@
 #!usr/bin/env python
-import RPi_IRIGB_offset as IRIGB
+import test_decoder2 as IRIGB
 import datetime
 import sys
+import os
+import subprocess
 
 filename = open('Pulse_Reader.txt', 'w')
 sys.stdout = filename
 
+
 pin = 12
 time_end = datetime.datetime.now() + datetime.timedelta(hours=12)   #How long you want it to run
-time_end += datetime.timedelta(seconds=3)			     #it takes 3 seconds to start so add 3 seconds to get an even dataset
-p = IRIGB.Offset_Decoder()
+time_end = time_end + datetime.timedelta(seconds=3)               # It takes 3 seconds to start running so add 3 seconds to total running time to get an even dataset
+
+p = IRIGB.Decoder()
 count = 0
+prev = ''
 pi = ''
-irig = ''
+os.nice(-20)
+os.setpriority(os.PRIO_PROCESS, 0, -20)
 
 while(datetime.datetime.now() <= time_end):
-	
-	pi = p.pi							# collect pi timestamp value
-	irig = p.get_decoded_data(pin)					# collect decoded IRIG-B timestamp
 
-	if pi == '':							# if there is no value for pi pass (the first value always 
-		pass							# reads in wrong since it starts in the middle of a pulse most of the time 
-	else:								# so this avoids printing output until a full pulse has occured)
-		if p.clock == '':
+    try:
 
-			print str(irig) + '   |   ' + str(pi)
+        pi = p.pi
+        p.get_decoded_data(pin)
 
-		else:
+    except OverflowError:
 
-			p.clock = ''
-			if count > 2:
-				print str(irig) + '   |   ' +  str(pi)	
+        pass
 
-	count += 1
-	
+    if count > 2:
+
+        if prev != '':
+                     
+            if p.clocktime != prev + datetime.timedelta(seconds=1):
+ 
+                p.clocktime = prev + datetime.timedelta(seconds=1)
+       
+        if pi != '':
+
+            CPUtemp = subprocess.check_output(['cat', '/sys/class/thermal/thermal_zone0/temp'])
+            GPUtemp = subprocess.check_output(['vcgencmd', 'measure_temp'])
+            GPUtemp = float(GPUtemp[5:9])
+            CPUtemp = float(CPUtemp)/ 1000
+            print(p.clocktime, '|' ,pi,'|', CPUtemp, '|', GPUtemp)
+
+    prev = p.clocktime
+    p.clearlists()
+    p.reset()
+    count += 1
 filename.close()
