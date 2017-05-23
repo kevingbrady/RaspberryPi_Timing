bc74ba093e0d6e406c49af7ca4c474a1193d856d
kevingbrady <kgb@nist.gov>
2017-05-23 11:45:05 -0400

    Update Offset_Decoder.py

--- Decoders/Offset_Decoder.py
+++ Decoders/Offset_Decoder.py
@@ -6,12 +6,8 @@ import wiringpi as GPIO
 
 class Decoder():
 	
-    Year = 0
-    SBS = 0.0
     pi = ''
-    cyclelist = []
-    templist = []
-    CPUtemp = ''
+    
 
     def __init__(self):
 
@@ -26,14 +22,15 @@ class Decoder():
     def cycles(self, pin):
 
         high = 0
-        max = 23
+        max = 20
 
         while GPIO.digitalRead(pin) == 1:
-
+       
+            high += 1
             if high < max:
 
                 time.sleep(0.0002)
-                high += 1
+                
 
         return(high)
 
@@ -44,16 +41,16 @@ class Decoder():
         x = 0
       
         DaysOY = (pulsebits[third] * 1) + (pulsebits[third +1] * 2) + (pulsebits[third +2] * 4) + (pulsebits[third + 3] * 8) + (pulsebits[third + 5] * 10) + (pulsebits[third + 6] * 20) + (pulsebits[third + 7] * 40) + (pulsebits[third + 8] * 80) + (pulsebits[third + 9] * 100) +(pulsebits[third + 10] * 200)
-        self.Year = ((pulsebits[third + 18] * 1) + (pulsebits[third + 19] * 2) + (pulsebits[third + 20] * 4) + (pulsebits[third + 21] * 8) + (pulsebits[third + 23] * 10) + (pulsebits[third + 24] * 20) + (pulsebits[third + 25] * 40) + (pulsebits[third + 26] * 80)) + 2000
-        self.SBS = float((pulsebits[third + 45] * (2**0)) + (pulsebits[third + 46] * (2**1)) + (pulsebits[third + 47] * (2**2)) + (pulsebits[third + 48] * (2**3)) + (pulsebits[third + 49] * (2**4)) + (pulsebits[third + 50] * (2**5)) + (pulsebits[third + 51] * (2**6)) + (pulsebits[third + 52] * (2**7)) + (pulsebits[third + 53] * (2**8)) + (pulsebits[third + 54] * (2**9)) + (pulsebits[third + 55] * (2**10)) + (pulsebits[third + 56] * (2**11)) + (pulsebits[third + 57] * (2**12)) + (pulsebits[third + 58] * (2**13)) + (pulsebits[third + 59] * (2**14)) + (pulsebits[third + 60] * (2**15)) + (pulsebits[third + 61] * (2**16)))
+        Year = ((pulsebits[third + 18] * 1) + (pulsebits[third + 19] * 2) + (pulsebits[third + 20] * 4) + (pulsebits[third + 21] * 8) + (pulsebits[third + 23] * 10) + (pulsebits[third + 24] * 20) + (pulsebits[third + 25] * 40) + (pulsebits[third + 26] * 80)) + 2000
+        SBS = float((pulsebits[third + 45] * (2**0)) + (pulsebits[third + 46] * (2**1)) + (pulsebits[third + 47] * (2**2)) + (pulsebits[third + 48] * (2**3)) + (pulsebits[third + 49] * (2**4)) + (pulsebits[third + 50] * (2**5)) + (pulsebits[third + 51] * (2**6)) + (pulsebits[third + 52] * (2**7)) + (pulsebits[third + 53] * (2**8)) + (pulsebits[third + 54] * (2**9)) + (pulsebits[third + 55] * (2**10)) + (pulsebits[third + 56] * (2**11)) + (pulsebits[third + 57] * (2**12)) + (pulsebits[third + 58] * (2**13)) + (pulsebits[third + 59] * (2**14)) + (pulsebits[third + 60] * (2**15)) + (pulsebits[third + 61] * (2**16)))
 
-        if(self.Year % 4 == 0):
+        if(Year % 4 == 0):
 
-            if(self.Year % 100 != 0):
+            if(Year % 100 != 0):
 
                 pass
 
-            elif(self.Year % 400 == 0):
+            elif(Year % 400 == 0):
 
                 pass
 
@@ -63,8 +60,8 @@ class Decoder():
             DaysOY -= 1
 
         DOY = DaysOY * 86400
-        year = (365.25 * 86400) * (self.Year - 1970)
-        timestamp = DOY + year + self.SBS + (6 * 3600)
+        year = (365.25 * 86400) * (Year - 1970)
+        timestamp = DOY + year + SBS + (6 * 3600)
 		
         if 0 < timestamp < 2147483641:
  
@@ -75,32 +72,29 @@ class Decoder():
     def get_decoded_data(self, pin):
 
         clocktime = ''
-        templist = []
         piFlag = 0
         bitcount = 0
         numbits = []
         bits = []        
         high = 0
-        self.CPUtemp = subprocess.check_output('./ReadCPUTemp')
-
+        cyclelist = []
+	
         while True:
-
-            time.sleep(0.001)
 
             high = self.cycles(pin)
 
             if high > 0:
 
-                self.cyclelist.append(high)
+                cyclelist.append(high)
 
             
-            if high < 21: 
+            if high < 19: 
 
-                    if high < 2:
+                    if high < 1:
 
                         pass
                 
-                    elif high < 10:
+                    elif high < 9:
                   
                         bits.append(0)
                         bitcount += 1
@@ -112,20 +106,12 @@ class Decoder():
         
             else:
 
-                if len(numbits) == 10:
-
-                    numbits.append(0)
-		    if self.cyclelist[-1:] != [23]:
-			self.cyclelist[-1:] = [23]
-
-                else:
-
-                    numbits.append(bitcount)
-                    bitcount = 0
+                numbits.append(bitcount)
+                bitcount = 0
            
-	    if sum(self.cyclelist[-2:]) >= 40:
+	    if cyclelist[-2:] > [19]:
 			
-                if numbits[-1:] == [0]:
+                if cyclelist[-1:] > [19]:
 
                     if piFlag != 1:
 
@@ -140,7 +126,10 @@ class Decoder():
                     else:
 
                         del bits[:]
-    
-                    del numbits[:]
+			del cyclelist[:]
+                        del numbits[:]
+			
                     return(clocktime)
+	
+	    time.sleep(0.001)
 
